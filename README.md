b4d5084171acfa98f7f6034cfec89409d18c9d7c
gloveman <gloveman@postech.ac.kr>
2025-10-27 05:50:31 +0900

    feat: Controller 구현 - RacingController 클래스 완성 - 테스트 편의와 책임 분리를 위해 InputView로부터 totalRounds를 String으로 입력받아 유효성 검증

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 - 구현
 - [x] Model 구현
 - [x] Model 테스트 코드 작성
-- [ ] View 구현
-- [ ] Controller 구현
+- [x] View 구현
+- [x] Controller 구현
 - [ ] Controller 테스트 코드 작성
 - [ ] main 함수 완성
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
 
 import racingcar.model.Car;
@@ -22,7 +23,7 @@ public class RacingController {
   /**
    * View 인스턴스들을 입력받아 controller 생성
    *
-   * @param inputView 사용자 입력 담당 View
+   * @param inputView  사용자 입력 담당 View
    * @param outputView 결과 출력 담당 View
    */
   public RacingController(InputView inputView, OutputView outputView) {
@@ -35,30 +36,67 @@ public class RacingController {
    * 입력 -> 게임 생성 -> 실행 -> 결과 출력을 순차적으로 수행
    */
   public void run() {
+    RacingGame game = createNewGame();
+    int totalRounds = getTotalRounds();
 
+    runRounds(game, totalRounds);
+    outputView.printWinners(game.getWinners());
   }
 
   private RacingGame createNewGame() {
-
+    List<Car> cars = createCarsFromInput();
+    return new RacingGame(cars, () -> Randoms.pickNumberInRange(0, 9));
   }
 
   private List<Car> createCarsFromInput() {
+    List<String> carNames = inputView.readCarNames();
+    validateCarNames(carNames);
+
+    return carNames.stream()
+        .map(Car::new)
+        .toList();
 
   }
 
-  private void ValidateCarNames(List<String> carNames) {
-
+  void validateCarNames(List<String> carNames) {
+    HashSet<String> uniqueNames = new HashSet<>();
+    for (String carName : carNames) {
+      if (carName.length() > 5) {
+        throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+      }
+      if (carName.isEmpty()) {
+        throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+      }
+      if (!uniqueNames.add(carName)) {
+        //set에 이미 동일 원소가 존재하면 false를 반환하는 것을 이용
+        throw new IllegalArgumentException("자동차 이름이 중복됩니다.");
+      }
+    }
   }
 
-  private int getValidTotalRounds() {
-
+  private int getTotalRounds() {
+    String totalRounds = inputView.readTotalRounds();
+    validateTotalRounds(totalRounds);
+    return Integer.parseInt(totalRounds);
   }
 
-  private void ValidateTotalRounds(int totalRounds) {
-
+  void validateTotalRounds(String totalRounds) {
+    int totalRoundsNumber;
+    try {
+      totalRoundsNumber = Integer.parseInt(totalRounds);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("입력값이 숫자가 아니거나 정수 범위를 벗어납니다.");
+    }
+    if (totalRoundsNumber < 1) {
+      throw new IllegalArgumentException("라운드 횟수는 양수여야 합니다.");
+    }
   }
+
   private void runRounds(RacingGame game, int totalRounds) {
-
+    for (int round = 1; round <= totalRounds; round++) {
+      game.playRound();
+      outputView.printRondResult(game.getCars());
+    }
   }
 
 }
